381404f01774bac5b3ea5ba6d8a4a310e21ff96b
Don Sirivat <don@Dons-MacBook-Pro.local>
2024-04-17 09:08:19 -0700

    Initial project setup

--- App.tsx
+++ App.tsx
@@ -3,7 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>CALIBRATION</Text>
+      <Text>by fake software</Text>
     </View>
   );
 }
