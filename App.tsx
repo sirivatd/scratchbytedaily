e171b9bb4744c3c60a216aa119168bdeaf366ae7
Don Sirivat <don@Dons-MacBook-Pro.local>
2024-04-17 10:16:13 -0700

    Add test data button for db integration

--- App.tsx
+++ App.tsx
@@ -1,10 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect } from 'react';
+import { StyleSheet, Button, Text, View } from 'react-native';
+import { ref, set, onValue, getDatabase } from "firebase/database";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import firebaseConfig from "./firebaseConfig.js";
 
-export default function App() {
+const App = () => {
+  // Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+  const writeTestData = () => {
+    const dbRef = ref(database, "test-data");
+    set(dbRef, { value: "lets gooooooooooooo" });
+  };
+  const listenForChanges = () => {
+    const dbRef = ref(database, "test-data");
+    onValue(dbRef, (snapshot) => {
+      console.log("Data changed:", snapshot.val());
+    });
+  };
+
+  // Listen for any db changes under test-data
+  useEffect(() => {
+    listenForChanges();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>CALIBRATION</Text>
       <Text>by fake software</Text>
+      <Button
+        onPress={writeTestData}
+        title='Add test data'
+        color="#841584"
+        accessibilityLabel="Add test data for debugging button"
+      />
     </View>
   );
 }
@@ -17,3 +48,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
